623b46b6d81d21519a767f440ea897c7de4125d1
MidnightChips <midnightchips@gmail.com>
2018-06-04 00:47:06 +0000

    improve script

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+cd /var/www/repo
 git pull
-rm ./Packages
-touch ./Packages
-./dpkg-scanpackages debs ./debs > Packages
-bzip2 -c9 Packages > Packages.bz2
+rm /var/www/repo/Packages
+touch /var/www/repo/Packages
+/var/www/repo/dpkg-scanpackages debs /var/www/repo/debs > Packages
+bzip2 -c9 /var/www/repo/Packages > /var/www/repo/Packages.bz2
 echo "Updated Packages"
